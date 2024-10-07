8b0bef0417e8e5f39ebfecf88213d35adfae8b37
Ewa Stephen-Akoda <S2403212@ndonline.ac.uk>
2024-10-07 15:43:06 +0100

    added more features

--- Account Creator/Account_Creator.py
+++ Account Creator/Account_Creator.py
@@ -2,11 +2,30 @@ def generateUsername(fullName, startYear):
     #this subroutine will generate the username 
     #based on the students' first name, last name 
     #and their starting year
+    #turn Ronald smith and 2022 into 22SmithR
+    firstName = (input("enter your first name "))
+    lastName = str(input("enter your last name "))
+    startYear = str(input("enter your starting year"))
+    print(startYear[2:4]+lastName+firstName[0]) 
     username = "#######"
     return username
 
 def calculateYearGroup(username):
     #this subroutine will calculate the year group
+    startYear= int(input("enter your starting year i.e *2022"))
+    currentYear = int(input("enter the current year"))
+    yearGroup = (currentYear - startYear )
+    print("you've spent ",yearGroup ,"years")
+    if yearGroup is 1:
+        print("year 7")
+    elif yearGroup is 2 :
+        print("year 8")
+    elif yearGroup is 3 :
+          print("year 9")
+    elif yearGroup is 4:
+        print("year 10")
+    elif yearGroup is 5
+    
     #that the student is in based on their username
     yearGroup = "Year #"
     return yearGroup
@@ -26,10 +45,10 @@ def isPasswordSecure(password):
     return True
 
 def menu():
-    fullName = input("enter your full name (first and last name separated by a space): ")
-    startYear = input("enter your starting year (e.g. 2024):")
+    fullName = print("welcome ")
+    startYear = print("you would be prompted some question please answer correctly")
     username = generateUsername(fullName, startYear)
     yearGroup = calculateYearGroup(username)
     password = getPassword()
     
-#menu()
+menu()
