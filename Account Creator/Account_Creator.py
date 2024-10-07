d66f02de3709943f29bb1ab1f6bd6efc8d9e86dd
Ewa Stephen-Akoda <S2403212@ndonline.ac.uk>
2024-10-07 15:50:41 +0100

    added changes to the calculate year group

--- Account Creator/Account_Creator.py
+++ Account Creator/Account_Creator.py
@@ -12,19 +12,26 @@ def generateUsername(fullName, startYear):
 
 def calculateYearGroup(username):
     #this subroutine will calculate the year group
-    startYear= int(input("enter your starting year i.e *2022"))
+    startYear= int(input("enter your starting year "))
     currentYear = int(input("enter the current year"))
     yearGroup = (currentYear - startYear )
     print("you've spent ",yearGroup ,"years")
-    if yearGroup is 1:
-        print("year 7")
-    elif yearGroup is 2 :
-        print("year 8")
-    elif yearGroup is 3 :
-          print("year 9")
-    elif yearGroup is 4:
-        print("year 10")
-    elif yearGroup is 5
+    if yearGroup <= 1:
+        print(" you are in year 7")
+    elif yearGroup <= 2 :
+        print(" you are in year 8")
+    elif yearGroup <= 3 :
+          print(" you are in year 9")
+    elif yearGroup <= 4:
+        print(" you are in year 10")
+    elif yearGroup <= 5:
+        print (" you are in year 11")
+    elif yearGroup <= 6:
+        print(" you are in year 12")
+    elif yearGroup <= 7 :
+        print(" you are in year13")
+    else:
+        print("you are not in a year group")
     
     #that the student is in based on their username
     yearGroup = "Year #"
